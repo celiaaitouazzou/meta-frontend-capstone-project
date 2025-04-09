62c9172a9c921ddb39696b1626a1903cbceeb494
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-04-09 15:53:36 -0400

    Setting up the logo

--- reserve-table/src/App.js
+++ reserve-table/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import logo from './Logo.svg';
 import './App.css';
+import Header from './Header.js'
+import Nav from './Nav.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <Headers></Headers>
+      <img src = {logo} ></img>
+      <Header>
+      </Header>
       <Nav></Nav>
       <Main></Main>
       <Footer></Footer>
