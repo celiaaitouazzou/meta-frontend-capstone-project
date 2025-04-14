2c09a2080d9e05293b80335e710184eeabd37b01
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-04-14 16:26:35 -0400

    Installed Tailwind for Styling

--- reserve-table/src/App.js
+++ reserve-table/src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Header from './Header.js'
-import Nav from './Nav.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Nav></Nav>
       <Main></Main>
       <Footer></Footer>
     </div>
--- reserve-table/src/Header.js
+++ reserve-table/src/Header.js
@@ -1,7 +1,13 @@
 import logo from './Logo.svg';
+import Nav from './Nav.js';
 
 function Header(){
-    return  (<img src = {logo} ></img>);
+    return  (
+    <header>
+        <img src = {logo} ></img>
+        <Nav></Nav>
+    </header>
+);
 }
 
 export default Header;
