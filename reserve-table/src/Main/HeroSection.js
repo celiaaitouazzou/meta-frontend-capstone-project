614305eb72b347d64005f094fe5f4a8dd14cbead
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-04-19 22:57:49 -0400

    got a good version of the hero section

--- reserve-table/src/Main/HeroSection.js
+++ reserve-table/src/Main/HeroSection.js
@@ -4,22 +4,27 @@ import hero from './hero.png';
 
 function HeroSection() {
   return (
-      <Container className="d-md-flex flex-row flex-wrap flex-2 text-start px-2 text-wrap Hero-col-1" fluid="sm" >
-      <Col>
-         <h1 className='hero-h1'>Little Lemon</h1>
-         <h2 className='hero-h1'>Chicago</h2>
-          <p className='px-2'>
-            Tucked into the city’s edge with a breeze of coastal charm, Little Lemon is the brainchild of Italian brothers Adrian and Mario — a modern Mediterranean spot where old-world flavors meet effortlessly cool vibes.
-            Think bright citrus, bold herbs, wood-fired everything, and a wine list that knows how to party.
-            It’s casual, it’s intimate, it’s where the playlist hits just right and the plates are made to be shared.
-            Come for the hummus, stay for the limoncello.
-          </p>
-          <Button className='Hero-button bg-black' variant="outline-light">Reserve a Table</Button>
-      </Col>
-      <Col className='Hero-col-2'>
-        <Image src={hero} rounded id="hero-image" width={400} height={500}/>
-      </Col>
-      </Container>
+    <Container className="py-5" style={{backgroundColor: '#495E57' }}>
+      <Row className="align-items-center" >
+        <Col xs={12} md={6} className="mb-4 mb-md-0 text-start">
+          <div style={{ textAlign: 'justify', maxWidth: '600px', color: '#F4CE14'}}>
+            <h1 className="display-4 text-start h2 fw-bold text-start" style={{ fontFamily: "'Markazi Text', serif", color: '#F4CE14'}}>Little Lemon</h1>
+            <p className="lead fs-6" style={{ textAlign: 'justify' }}>
+              Tucked into the city’s edge with a breeze of coastal charm, Little Lemon is the brainchild of Italian brothers Adrian and Mario — a modern Mediterranean spot where old-world flavors meet effortlessly cool vibes.
+              Think bright citrus, bold herbs, wood-fired everything, and a wine list that knows how to party.
+              It’s casual, it’s intimate, it’s where the playlist hits just right and the plates are made to be shared.
+              Come for the hummus, stay for the limoncello.
+            </p>
+            <Button className="Hero-button text-white bg-dark border-light shadow-none mt-3">
+              Reserve A Table
+            </Button>
+          </div>
+        </Col>
+        <Col xs={12} md={6} className="position-relative">
+            <Image src={hero} fluid rounded style={{height: '500px', width: 'auto', objectFit: 'cover'}}/>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
