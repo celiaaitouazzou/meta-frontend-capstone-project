7da7297dbc01a07afe6041849b8fdf8346a0f960
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-04-20 20:29:33 -0400

    Starting Hightlight Component

--- reserve-table/src/Main/Highlight.js
+++ reserve-table/src/Main/Highlight.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Button, Container,Row } from 'react-bootstrap'
 
 function Highlight() {
   return (
-    <div>Highlight</div>
+    <Container className='container-style'>
+      <Row className='d-flex'>
+        <h1>Specials</h1>
+        <Button className='button-online-menu'>Online Menu</Button>
+      </Row>
+      <Row>
+      </Row>
+    </Container>
   )
 }
 
