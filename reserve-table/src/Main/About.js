babd621d1c9a80a563a3345a62b930d5284633d5
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-04-29 23:32:57 -0400

    styling about

--- reserve-table/src/Main/About.js
+++ reserve-table/src/Main/About.js
@@ -14,32 +14,34 @@ function About() {
         setIndex(selectedIndex);
     };
     return (
-        <Container style={{display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+        <Container style={{display:'flex',flexDirection:'row',justifyContent:'space-between',margin:"5%"}}>
             <div>
                 <h1>About</h1>
                 <p>In the sun-drenched hills of Italy, two boys, Adrian and Mario, grew up amidst the aroma of olive groves and the vibrant flavors of the Mediterranean. Adrian, with a passion for the rustic dishes of his Nonna's kitchen, and Mario, captivated by the art of crafting the perfect pasta, shared a dream: to bring the taste of their homeland to the world.</p>
                 <p>Drawn by the promise of opportunity, they journeyed to Chicago, a city with a rich history of Italian immigration dating back to the 1850s. They found a community that welcomed them, but missed the authentic flavors of their upbringing. Inspired by the tradition of Italian immigrants who sought to preserve their culture in a new land, they decided to share their heritage through food.</p>
                 <p>And so, Little Lemon was born. More than just a restaurant, it was a celebration of their shared history and the diverse tapestry of Mediterranean cuisine. Each dish, from Adrian's hearty stews to Mario's delicate handmade pasta, told a story of sun-drenched Italian landscapes and the warmth of family gatherings. Little Lemon became a place where Chicagoans could experience the true taste of Italy, a testament to Adrian and Mario's journey and their enduring love for their culture.</p>
             </div>
-                <Carousel activeIndex={index} onSelect={handleSelect}>
+            <div>
+            <Carousel data-bs-theme="dark">
                 <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src={slide1}
-                alt="Adrian and Mario"
-                />
+                    <img
+                    className="d-block w-100"
+                    src={slide1}
+                    alt="First slide"
+                    />
                     <Carousel.Caption>
-                    <h3>Mario and Adrian</h3>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                     className="d-block w-100"
                     src={slide2}
-                    alt="Adrian and Mario"
+                    alt="Second slide"
                     />
                     <Carousel.Caption>
-                    <h3>Mario and Adrian</h3>
+                    <h5>Second slide label</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -47,18 +49,16 @@ function About() {
                     <img
                     className="d-block w-100"
                     src={slide3}
-                    alt="Adrian"
+                    alt="Third slide"
                     />
                     <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <h5>Third slide label</h5>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                     </p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 </Carousel>
-            <div>
-
             </div>
         </Container>
     )
