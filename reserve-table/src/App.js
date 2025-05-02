80b3d06f0f0440b1560c55bfb628e48926349da3
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-05-02 15:55:49 -0400

    working on routing

--- reserve-table/src/App.js
+++ reserve-table/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navigation from './Navigation';
 import Main from './Main';
 import Footer from'./Footer.js'
+import React from 'react';
+import AppRoutes from './AppRoutes.js';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <Navigation />
       <Main />
       <Footer/>
+      <AppRoutes />
     </div>
   );
 }
--- reserve-table/src/Navigation.js
+++ reserve-table/src/Navigation.js
@@ -1,33 +1,37 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from './logo.jpg'
+import logo from './logo.jpg';
+import AppRoutes from './AppRoutes'; // Import the new component
 
 function Navigation() {
   return (
-    <Navbar expand="lg" className="karla-navbar sticky-bar" sticky='top'>
+    <Router> {/* Wrap the entire Navigation component with Router */}
+      <Navbar expand="lg" className="karla-navbar sticky-bar" sticky='top'>
         <Container className="nav-container d-flex custom-flex-nav">
-            <div className="d-lg-none mx-auto">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            </div>
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Navbar.Brand href="/">
-                    <img src={logo} width="150" height="75%" class="float-lg-left" className="d-inline-block" alt="Little Lemon logo"/>
-                </Navbar.Brand>
-                <Nav className="ms-auto ">
-                    <Nav.Link href="/"  className="text-dark">HOME</Nav.Link>
-                    <Nav.Link href="/About" className="text-dark">ABOUT</Nav.Link>
-                    <Nav.Link href="/Menu"  className="text-dark">MENU</Nav.Link>
-                    <Nav.Link href="/Reservation" className="text-dark">RESERVATION</Nav.Link>
-                    <Nav.Link href="/OrderOnline" className="text-dark">ORDER ONLINE</Nav.Link>
-                    <Nav.Link href="/Login" className="text-dark">LOGIN</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+          <div className="d-lg-none mx-auto">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Brand as={Link} to="/">
+              <img src={logo} width="150" height="75%" className="float-lg-left d-inline-block" alt="Little Lemon logo" />
+            </Navbar.Brand>
+            <Nav className="ms-auto">
+              <Link to="/" className="nav-link text-dark">HOME</Link>
+              <Link to="/about" className="nav-link text-dark">ABOUT</Link>
+              <Link to="/menu" className="nav-link text-dark">MENU</Link>
+              <Link to="/reservation" className="nav-link text-dark">RESERVATION</Link>
+              <Link to="/order-online" className="nav-link text-dark">ORDER ONLINE</Link>
+              <Link to="/login" className="nav-link text-dark">LOGIN</Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-    </Navbar>
-  )
+      </Navbar>
+      <AppRoutes /> {/* Render the separate component for routes */}
+    </Router>
+  );
 }
 
-export default Navigation
+export default Navigation;
