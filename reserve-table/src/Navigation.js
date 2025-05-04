12ef7cc5883e9670fe27a7b3a7d6f87407d9be32
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-05-03 23:17:05 -0400

    finished routing but weird styling so fixing styling

--- reserve-table/src/Navigation.js
+++ reserve-table/src/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 import logo from './logo.jpg'
 
 function Navigation() {
@@ -17,12 +17,12 @@ function Navigation() {
                     <img src={logo} width="150" height="75%" class="float-lg-left" className="d-inline-block" alt="Little Lemon logo"/>
                 </Navbar.Brand>
                 <Nav className="ms-auto ">
-                    <Nav.Link href="/"  className="text-dark">HOME</Nav.Link>
-                    <Nav.Link href="/About" className="text-dark">ABOUT</Nav.Link>
-                    <Nav.Link href="/Menu"  className="text-dark">MENU</Nav.Link>
-                    <Nav.Link href="/Reservation" className="text-dark">RESERVATION</Nav.Link>
-                    <Nav.Link href="/OrderOnline" className="text-dark">ORDER ONLINE</Nav.Link>
-                    <Nav.Link href="/Login" className="text-dark">LOGIN</Nav.Link>
+                    <Link to={"/"}  className="text-dark">HOME</Link>
+                    <Link to={"/About"} className="text-dark">ABOUT</Link>
+                    <Link to={"/Menu"}  className="text-dark">MENU</Link>
+                    <Link to={"/Reservation"} className="text-dark">RESERVATION</Link>
+                    <Link to={"/OrderOnline"} className="text-dark">ORDER ONLINE</Link>
+                    <Link to={"/Login"} className="text-dark">LOGIN</Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
--- reserve-table/src/index.js
+++ reserve-table/src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from './Navigation';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import AboutPage from './Pages/AboutPage.js'
+import Menu from './Pages/Menu.js'
+import Reservation from './Pages/Reservation.js'
+import OrderOnline from './Pages/OrderOnline.js'
+import Login from './Pages/Login.js'
+
+
+const router = createBrowserRouter([
+  {path:'/',element:<App />},
+  {path:'/About',element:<AboutPage />},
+  {path:'/Menu',element:<Menu />},
+  {path:'/Reservation',element:<Reservation />},
+  {path:'/OrderOnline',element:<OrderOnline />},
+  {path:'/Login',element:<Login />}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
