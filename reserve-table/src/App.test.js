642715aa77c02d75e233c8472dec3f7d776633ec
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-05-13 23:46:41 -0400

    still testing

--- reserve-table/src/App.test.js
+++ reserve-table/src/App.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { Link } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
--- reserve-table/src/setupTests.js
+++ reserve-table/src/setupTests.js
@@ -3,9 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// Mock react-router-dom globally
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'), // Keep all actual functionalities
-  Link: ({ children, to }) => <a href={to}>{children}</a>, // Mock Link as an anchor tag
-}));
