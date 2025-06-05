7739064a165df0e79f62e21c98aa9b4cfb0e4255
celiaaitouazzou <celiaaitouazzou@gmail.com>
2025-06-04 21:24:06 -0400

    finished fixing the database

--- reserve-table/src/firebase.js
+++ reserve-table/src/firebase.js
@@ -1,36 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-
-  apiKey: "",
-
-  authDomain: "little-lemon-reservation-400f0.firebaseapp.com",
-
-  projectId: "little-lemon-reservation-400f0",
-
-  storageBucket: "little-lemon-reservation-400f0.firebasestorage.app",
-
-  messagingSenderId: "993698340944",
-
-  appId: "1:993698340944:web:5df86825cc1ed6f634b155",
-
-  measurementId: "G-Z3GQRSM6KR"
-
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
